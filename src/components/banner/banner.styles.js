cbd3cc257e7358cdcf04c81e9f40f82a9d3106e7
matthewvero <mattavero@hotmail.com>
2020-06-17 16:58:02 +0100

    mobile UI tweaks

--- src/components/banner/banner.styles.js
+++ src/components/banner/banner.styles.js
@@ -4,7 +4,7 @@ import styled from 'styled-components'
 
 
 export const BannerBackground = styled.div`
-      width: 100%;
+      width: 101%;
       height: 100%;
       background-image: url(${props => props.backgroundImage});
       background-position: center;
